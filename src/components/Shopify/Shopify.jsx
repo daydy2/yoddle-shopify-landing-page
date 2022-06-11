20d5e6cabb30fa5b10f7549d0f74fc6a89808ba1
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-06-11 04:17:20 +0100

    feat: responsive img

--- src/components/Shopify/Shopify.jsx
+++ src/components/Shopify/Shopify.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import playIcon from '../../img/icons8-play-24.png'
+import sam1 from '../../img/sam1.jpg'
+import sam2 from '../../img/sam2.jpg'
+import sam3 from '../../img/sam3.jpg'
+import sam4 from '../../img/sam4.jpg'
+import sam5 from '../../img/sam5.jpg'
 import './shopify.css'
 
 const Shopify = () => {
@@ -38,6 +43,36 @@ const Shopify = () => {
                 
               </div>
            
+          </section>
+
+          <section className="section2">
+            <div className="section2__div-adText">
+              <h1 className="section2__div-h1Text">Bring your business online</h1>
+              <p className='section2__div-pText'>Create an ecommerce website backed by powerful tools that help you find customers, drive sales, and manage your day-to-day.</p>
+              <p className='setion2__div-pExample'>
+                Explore more examples
+                <span className="section2__div-pExample-icon">
+                  <i class="fa-solid fa-arrow-right"></i>
+                </span>
+              </p>
+              <ul className="section2__ul">
+                <li className="section2__ul-listItem">
+                  <img src="" alt="" srcset="" />
+                </li>
+                <li className="section2__ul-listItem">
+                  <img src="" alt="" srcset="" />
+                </li>
+                <li className="section2__ul-listItem">
+                  <img src="" alt="" srcset="" />
+                </li>
+                <li className="section2__ul-listItem">
+                  <img src="" alt="" srcset="" />
+                </li>
+                <li className="section2__ul-listItem">
+                  <img src="" alt="" srcset="" />
+                </li>                
+              </ul>
+            </div>
           </section>
           
         </main>
