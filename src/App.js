1b9406c616fd37cbbbbe9f67e22efd129c6be306
igedanielolumide@gmail.com <igedanielolumide@gmail.com>
2022-06-19 06:09:59 +0100

    feat: added spinning globe - cobe package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cobe from './components/Cobe/Cobe';
 import Shopify from './components/Shopify/Shopify';
 
 function App() {
   return (
     <div className="App">
-      <Shopify />
+      {/* <Shopify /> */}
+      <Cobe />
     </div>
   );
 }
